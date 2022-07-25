2c98b534e6ff5a9b926ddb43bca8e28acce782e1
MaxD <max.moderation@gmail.com>
2022-07-25 23:35:33 +0300

    feat: end objects task

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -51,8 +51,9 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const jsonObj = JSON.parse(json);
+  return Object.setPrototypeOf(jsonObj, proto);
 }
 
 
